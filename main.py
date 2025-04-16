5395fec3cf4c6428e7c8dff33eb9b72d8b92638b
GuanSojaSinantin <guansojasinantin@gmail.com>
2025-04-16 11:47:05 +0700

    add button and column

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import streamlit as st
 
 st.title("My first WebApp")
 st.divider()
+
+col1, col2 = st.columns(2)
+with col1:
+    st.button("left button")
+with col2:
+    st.button("Right button")
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
